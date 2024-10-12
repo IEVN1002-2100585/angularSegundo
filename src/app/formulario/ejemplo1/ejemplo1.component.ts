8b6f0a2a306cb0f405dca4ee66aedeffcffb2221
Luz Cristina Dominguez Zendejas <21000585@alumnos.utleon.edu.mx>
2024-10-12 10:26:14 -0600

    Rutas

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -11,18 +11,26 @@ interface Usuarios{
 @Component({
   selector: 'app-ejemplo1',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, Ejemplo1Component],
   templateUrl: './ejemplo1.component.html',
   styleUrl: './ejemplo1.component.css'
 })
 export class Ejemplo1Component implements OnInit{
   formGroup!:FormGroup;
 
+  materia='pwa'
+  tem:string =''
+  alumnos:Usuarios={
+    nombre: '',
+    edad: 0,
+    email: ''
+  }
+
   constructor( private fb:FormBuilder){}
   ngOnInit(): void {
     this.formGroup=this.initForm();
   }
-  initForm():void{
+  initForm():FormGroup{
     return this.fb.group({
       nombre:[''],
       edad:[''],
@@ -31,7 +39,25 @@ export class Ejemplo1Component implements OnInit{
   }
 
   OnSubmit():void{
+    const{nombre,edad,email}=this.formGroup.value;
+    this.alumnos.nombre=nombre;
+    this.alumnos.edad=edad;
+    this.alumnos.email=email;
+    let alumnoJSON=JSON.stringify(this.alumnos);
     console.log(this.formGroup.value);
+    localStorage.setItem('materia', this.materia); //Sirve para almacenar mi sprint
+    localStorage.setItem('alumno', alumnoJSON);
+  }
+
+  subImprimir():void{
+    this.tem=localStorage.getItem('materia')!
+    console.log(this.alumnos);
+
+    const alumnoGuardado=localStorage.getItem('alumno')
+  if (alumnoGuardado) {
+    const alumno:Usuarios=JSON.parse(alumnoGuardado)
+  }
+  
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ const { Component } = require('@angular/core');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/app.component.html.{html,ts}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [ejemplo1],
+  plugins: [],
 }
